011a5d9905dd366c69979ad9bf73a09e0a98f3f7
AngelMercado <myjava@outlook.es>
2017-09-26 22:21:34 -0500

    Feat: CommentService
    Added commentService into commentComponent to add new comments.
    this feature requires the user have active token.

--- angular2rc3/app/components/comment.component.ts
+++ angular2rc3/app/components/comment.component.ts
@@ -1,24 +1,29 @@
 // Importar el núcleo de Angular
-import {Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES, Router, ActivatedRoute } from '@angular/router'
-import {LoginService} from '../service/login.service';
-import {User} from "../model/user";
-import {Video} from "../model/video";
+import { LoginService } from '../service/login.service';
+import { CommentService } from '../service/comment.service';
+import { User } from "../model/user";
+import { Video } from "../model/video";
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'comments',
     templateUrl: 'app/view/comments.html',
     directives: [ROUTER_DIRECTIVES],    
-    providers: [LoginService]
+    providers: [LoginService,CommentService]
 })
  
 // Clase del componente donde irán los datos y funcionalidades
 export class CommentComponent implements OnInit{ 	
 	
 	public titleComments:string = "Comentarios";
-	public comment;
+	public comment={};
 	public identity;
+	public status;
+	public 	errorMessage;
+
 	constructor(private _loginService:LoginService,
+				private _commentService:CommentService,
 				private _router:Router,
 				private _route:ActivatedRoute){}
 
@@ -41,7 +46,31 @@ export class CommentComponent implements OnInit{
 	}
 
 	onSubmit(){
-		console.log(this.comment);
+		console.log("algo");
+		let token = this._loginService.getToken();
+		this._commentService.create(token,this.comment).subscribe(
+			response =>{
+					console.log(response);
+
+					this.status = (response.status != null) ? response.status : null;
+					if(this.status != "success"){
+						this.status = "error";
+					}else{
+						this.comment = {
+							"video_id" :"",
+							"body": "";
+						};
+						//console.log(this.comment);
+					}						
+				},
+			error => {
+				console.log("Error en la peticion");					
+				this.errorMessage = <any> error;
+				if(this.errorMessage != null){
+					console.log("Error en la peticion");
+				}
+			});
+
 	}
 
 
